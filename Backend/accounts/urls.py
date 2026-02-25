53c8ce49cb645e1c8ab315ee655061d0019218ba
BuckerooBanzaiV2 <jan.matejsek.24@mosbach.dhbw.de>
2026-02-25 15:34:03 +0100

    WIP-00003-FixedJWTTokenRotation

--- Backend/accounts/urls.py
+++ Backend/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-from accounts.views import RegisterView
+from accounts.views import RegisterView, TokenRefreshView
 
 urlpatterns = [
     path("register", RegisterView.as_view(), name="register"),
--- Backend/accounts/views.py
+++ Backend/accounts/views.py
@@ -1,4 +1,7 @@
+from django.contrib.auth import get_user_model
 from rest_framework import generics, permissions
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+from rest_framework_simplejwt.views import TokenRefreshView as BaseTokenRefreshView
 
 from accounts.serializers import RegisterSerializer
 
@@ -6,3 +9,17 @@ from accounts.serializers import RegisterSerializer
 class RegisterView(generics.CreateAPIView):
     permission_classes = [permissions.AllowAny]
     serializer_class = RegisterSerializer
+
+
+class TokenRefreshView(BaseTokenRefreshView):
+    """Wraps simplejwt's TokenRefreshView to handle User.DoesNotExist
+    as a proper 401 instead of an unhandled 500.
+    With ROTATE_REFRESH_TOKENS=True simplejwt looks up the user in the DB;
+    if the user no longer exists the token is effectively invalid.
+    """
+
+    def post(self, request, *args, **kwargs):
+        try:
+            return super().post(request, *args, **kwargs)
+        except get_user_model().DoesNotExist:
+            raise InvalidToken("Token contained no recognisable user identification")
